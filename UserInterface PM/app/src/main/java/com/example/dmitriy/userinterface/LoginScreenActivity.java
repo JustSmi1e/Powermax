bfea58858f3787a530265a45e0bd9dc62b7aa7da
JustSmi1e <Just_smi1e@mail.ru>
2015-07-30 23:25:52 +0300

    UserCode and Login windows

--- UserInterface PM/app/src/main/java/com/example/dmitriy/userinterface/LoginScreenActivity.java
+++ UserInterface PM/app/src/main/java/com/example/dmitriy/userinterface/LoginScreenActivity.java
@@ -1,19 +1,43 @@
 package com.example.dmitriy.userinterface;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class LoginScreenActivity extends Activity {
+
+    private View.OnFocusChangeListener ofchange = new View.OnFocusChangeListener() {
+        @Override
+        public void onFocusChange(View v, boolean hasFocus) {
+            if((v.getId() == R.id.host_address || v.getId() == R.id.panel_id) && !hasFocus ){
+                InputMethodManager imm =  (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+            }
+            else if ((v.getId() == R.id.host_address || v.getId() == R.id.panel_id) && hasFocus ){
+                InputMethodManager imm =  (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.showSoftInput(v, InputMethodManager.SHOW_IMPLICIT);
+
+            }
+        }
+    }; ///set OnFocusChangeListener to close keyboard when clicked on the empty space
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.credentials_screen_layout);
+        EditText address = (EditText)findViewById(R.id.host_address);
+        EditText panelID = (EditText)findViewById(R.id.panel_id);
+        address.setOnFocusChangeListener(ofchange);
+        panelID.setOnFocusChangeListener(ofchange);
 
     }
 
--- UserInterface PM/app/src/main/java/com/example/dmitriy/userinterface/UserCodeActivity.java
+++ UserInterface PM/app/src/main/java/com/example/dmitriy/userinterface/UserCodeActivity.java
@@ -1,15 +1,21 @@
 package com.example.dmitriy.userinterface;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.gesture.GestureOverlayView;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.GestureDetector;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View.OnClickListener;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.BaseInputConnection;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -19,29 +25,33 @@ public class UserCodeActivity extends Activity implements OnClickListener{
     EditText text;
     ViewGroup group;
     BaseInputConnection textFileInputConnection;
-    ///final Button button1 = (Button)findViewById(R.id.digit1_button);
-    ///final Button button2 = (Button)findViewById(R.id.digit2_button);
-    ///final Button button3 = (Button)findViewById(R.id.digit3_button);
-    ///final Button button4 = (Button)findViewById(R.id.digit4_button);
-    ///final Button button5 = (Button)findViewById(R.id.digit5_button);
-    ///final Button button6 = (Button)findViewById(R.id.digit6_button);
-    ///final Button button7 = (Button)findViewById(R.id.digit7_button);
-    ///final Button button8 = (Button)findViewById(R.id.digit8_button);
-    ///final Button button9 = (Button)findViewById(R.id.digit9_button);
-    ///final Button button_log = (Button)findViewById(R.id.digit0_button);
-    ///final Button button_back = (Button)findViewById(R.id.digit0_button);
+    private View.OnTouchListener otl = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            return true;
+        }
+    }; ///set onTouchListener to block keyboard when text is focused;
 
-
-
+    private View.OnLongClickListener olc = new View.OnLongClickListener() {
+        @Override
+        public boolean onLongClick(View v) {
+            if (v.getId() == R.id.backspace_button){
+                EditText edit = (EditText)findViewById(R.id.user_code_textview);
+                edit.setText("");
+            }
+            return false;
+        }
+    };//set onLongClickListener to clear code field when backspace is pressed for a brief time
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_code_screen_layout);
-        ///button0 = (Button)findViewById(R.id.digit0_button);
         text = (EditText)findViewById(R.id.user_code_textview);
-        text.requestFocus();
+        text.setOnTouchListener(otl);
+        Button backspace = (Button)findViewById(R.id.backspace_button);
+        backspace.setOnLongClickListener(olc);
         textFileInputConnection = new BaseInputConnection(text, true);
         group = (ViewGroup)findViewById(R.id.button_grid);
         View v;
@@ -49,7 +59,6 @@ public class UserCodeActivity extends Activity implements OnClickListener{
             v = group.getChildAt(i);
             if(v instanceof Button) v.setOnClickListener(this);
         }
-        ///button0.setOnClickListener(this);
     }
 
     @Override
@@ -79,20 +88,35 @@ public class UserCodeActivity extends Activity implements OnClickListener{
 
         switch (v.getId()){
             case R.id.digit0_button: text.append("0");
+                break;
             case R.id.digit1_button: text.append("1");
+                break;
             case R.id.digit2_button: text.append("2");
+                break;
             case R.id.digit3_button: text.append("3");
+                break;
             case R.id.digit4_button: text.append("4");
+                break;
             case R.id.digit5_button: text.append("5");
+                break;
             case R.id.digit6_button: text.append("6");
+                break;
             case R.id.digit7_button: text.append("7");
+                break;
             case R.id.digit8_button: text.append("8");
+                break;
             case R.id.digit9_button: text.append("9");
+                break;
             case R.id.backspace_button: textFileInputConnection.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DEL));
-
-
-
+               break;
+            case R.id.login_button_user: Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
         }
 
     }
+
+
+
+
 }
